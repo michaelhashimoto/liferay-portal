ee0e6099059e4159b77f649923c3eeaa23687414
Brian Chiu <brian.chiu@liferay.com>
2011-08-02 09:50:21 -0700

    Test Fixes - portlet/breadcrumb

--- portal-web/test/com/liferay/portalweb/portlet/breadcrumb/portlet/addportletchildpage/AddChildPageBreadcrumbTest.java
+++ portal-web/test/com/liferay/portalweb/portlet/breadcrumb/portlet/addportletchildpage/AddChildPageBreadcrumbTest.java
@@ -23,6 +23,8 @@ import com.liferay.portalweb.portal.util.RuntimeVariables;
 public class AddChildPageBreadcrumbTest extends BaseTestCase {
 	public void testAddChildPageBreadcrumb() throws Exception {
 		selenium.open("/web/guest/home/");
+		selenium.clickAt("//div[@id='dockbar']",
+			RuntimeVariables.replace("Dockbar"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
@@ -30,7 +32,7 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 			}
 
 			try {
-				if (selenium.isVisible("link=Breadcrumb Test Page")) {
+				if (selenium.isElementPresent("//div[4]/div/ul/li[1]/a")) {
 					break;
 				}
 			}
@@ -41,35 +43,8 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 		}
 
 		selenium.saveScreenShotAndSource();
-		selenium.clickAt("link=Breadcrumb Test Page",
-			RuntimeVariables.replace(""));
-		selenium.waitForPageToLoad("30000");
-		selenium.saveScreenShotAndSource();
-		selenium.clickAt("main-content", RuntimeVariables.replace(""));
-		selenium.clickAt("navigation", RuntimeVariables.replace(""));
-		selenium.clickAt("dockbar", RuntimeVariables.replace(""));
-
-		for (int second = 0;; second++) {
-			if (second >= 60) {
-				fail("timeout");
-			}
-
-			try {
-				if (selenium.isElementPresent("//div/div[3]/div/ul/li[1]/a")) {
-					break;
-				}
-			}
-			catch (Exception e) {
-			}
-
-			Thread.sleep(1000);
-		}
-
-		selenium.saveScreenShotAndSource();
-		selenium.clickAt("//div/div[3]/div/ul/li[1]/a",
+		selenium.clickAt("//div[4]/div/ul/li[1]/a",
 			RuntimeVariables.replace("Manage Pages"));
-		selenium.waitForPageToLoad("30000");
-		selenium.saveScreenShotAndSource();
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
@@ -77,7 +52,7 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 			}
 
 			try {
-				if (RuntimeVariables.replace("Liferay")
+				if (RuntimeVariables.replace("Public Pages")
 										.equals(selenium.getText(
 								"//div/div[3]/a"))) {
 					break;
@@ -90,9 +65,8 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 		}
 
 		selenium.saveScreenShotAndSource();
-		selenium.clickAt("link=Children", RuntimeVariables.replace(""));
-		selenium.waitForPageToLoad("30000");
-		selenium.saveScreenShotAndSource();
+		selenium.clickAt("//div[@id='_88_layoutsTreeOutput']/ul/li/div/div[1]",
+			RuntimeVariables.replace("Drop Down Arrow"));
 
 		for (int second = 0;; second++) {
 			if (second >= 60) {
@@ -100,9 +74,7 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 			}
 
 			try {
-				if (RuntimeVariables.replace("Liferay")
-										.equals(selenium.getText(
-								"//div/div[3]/a"))) {
+				if (selenium.isVisible("//ul/li[2]/div/div[3]/a")) {
 					break;
 				}
 			}
@@ -113,11 +85,36 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 		}
 
 		selenium.saveScreenShotAndSource();
-		selenium.type("_88_name_en_US",
+		assertEquals(RuntimeVariables.replace("Breadcrumb Test Page"),
+			selenium.getText("//ul/li[2]/div/div[3]/a"));
+		selenium.clickAt("//ul/li[2]/div/div[3]/a",
+			RuntimeVariables.replace("Breadcrumb Test Page"));
+		Thread.sleep(5000);
+		selenium.clickAt("//div/span/button[1]",
+			RuntimeVariables.replace("Add Child Page"));
+
+		for (int second = 0;; second++) {
+			if (second >= 60) {
+				fail("timeout");
+			}
+
+			try {
+				if (selenium.isVisible("//input[@id='_88_name_en_US']")) {
+					break;
+				}
+			}
+			catch (Exception e) {
+			}
+
+			Thread.sleep(1000);
+		}
+
+		selenium.saveScreenShotAndSource();
+		selenium.type("//input[@id='_88_name_en_US']",
 			RuntimeVariables.replace("Child Test Page"));
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("//input[@value='Add Page']",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace("Add Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
 		assertEquals(RuntimeVariables.replace(
@@ -142,11 +139,12 @@ public class AddChildPageBreadcrumbTest extends BaseTestCase {
 
 		selenium.saveScreenShotAndSource();
 		selenium.clickAt("link=Breadcrumb Test Page",
-			RuntimeVariables.replace(""));
+			RuntimeVariables.replace("Breadcrumb Test Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
 		assertTrue(selenium.isElementPresent("link=Child Test Page"));
-		selenium.clickAt("link=Child Test Page", RuntimeVariables.replace(""));
+		selenium.clickAt("link=Child Test Page",
+			RuntimeVariables.replace("Child Test Page"));
 		selenium.waitForPageToLoad("30000");
 		selenium.saveScreenShotAndSource();
 	}
