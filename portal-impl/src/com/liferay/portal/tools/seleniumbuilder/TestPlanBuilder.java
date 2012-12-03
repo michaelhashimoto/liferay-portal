721e0ab730f7cd7d2b620d418b645a1c0c072d2f
Michael Hashimoto <michael.hashimoto@liferay.com>
2012-12-03 09:41:51 -0800

    LPS-31398

--- portal-impl/src/com/liferay/portal/tools/seleniumbuilder/TestPlanBuilder.java
+++ portal-impl/src/com/liferay/portal/tools/seleniumbuilder/TestPlanBuilder.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.portal.tools;
+package com.liferay.portal.tools.seleniumbuilder;
 
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.StringBundler;
