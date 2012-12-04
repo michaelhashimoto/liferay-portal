2a5b42c5a7ade957091030999c1f002f033a5464
Michael Hashimoto <michael.hashimoto@liferay.com>
2012-12-04 10:40:12 -0800

    LPS-31401

--- portal-web/test/com/liferay/portalweb/blocks/styles/scripts.js
+++ portal-web/test/com/liferay/portalweb/blocks/styles/scripts.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-	$(".expand-macro-steps > a").click(function () {
+	$(".expand-macro-steps > a").click(function() {
 		if ($(this).parent().children('.macro-steps:first').is(":hidden")) {
 			$(this).parent().children('.macro-steps:first').slideDown("fast");
 		} else {
 			$(this).parent().children('.macro-steps:first').slideUp("fast");
 		}
 		event.preventDefault();
-	})	
+	})
 });
