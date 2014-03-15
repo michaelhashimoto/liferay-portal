ae397d76c9a5377648af05c115d8d40ddea4136b
Zsolt Balogh <zsolt.balogh@liferay.com>
2014-03-15 12:21:44 +0100

    SF

--- portal-web/test/functional/com/liferay/portalweb/portal/util/EmailCommands.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/EmailCommands.java
@@ -25,8 +25,8 @@ import javax.mail.Session;
 import javax.mail.Store;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
+import javax.mail.internet.MimeMessage;
 
 /**
  * @author Kwang Lee
