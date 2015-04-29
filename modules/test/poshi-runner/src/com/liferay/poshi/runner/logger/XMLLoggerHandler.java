cdb54cb5f71a9e5322184df2ac9e7cf6872eb725
Leslie Wong <leslie.wong@liferay.com>
2015-04-29 10:28:18 -0700

    LRQA-14420 Modify _getLoggerElementFromElement to use new method

--- modules/test/poshi-runner/src/com/liferay/poshi/runner/logger/XMLLoggerHandler.java
+++ modules/test/poshi-runner/src/com/liferay/poshi/runner/logger/XMLLoggerHandler.java
@@ -252,16 +252,17 @@ public final class XMLLoggerHandler {
 	}
 
 	private static LoggerElement _getLoggerElementFromElement(Element element) {
-		String elementName = element.getName();
+		LoggerElement loggerElement = _getLineGroupLoggerElement(element);
 
-		LoggerElement loggerElement = new LoggerElement();
+		LoggerElement childContainerLoggerElement =
+			_getChildContainerLoggerElement();
 
-		if (elementName.equals("description") || elementName.equals("echo")) {
-			loggerElement = _getEchoLoggerElement(element);
-		}
-		else if (elementName.equals("fail")) {
-			loggerElement = _getFailLoggerElement(element);
-		}
+		loggerElement.addChildLoggerElement(
+			_getChildContentLoggerElement(
+				childContainerLoggerElement, element));
+
+		loggerElement.addChildLoggerElement(
+			_getClosingLineContainerLoggerElement(element));
 
 		return loggerElement;
 	}
