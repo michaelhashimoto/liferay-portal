e3c8bc010ff10f4fb743c48fa6d7d4c23c89d003
Brian Chan <brian.chan@liferay.com>
2015-05-06 20:49:20 -0700

    LRQA-11339 SF to fix warning: create pattern as global var: portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java 1471

--- portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
+++ portal-web/test/functional/com/liferay/portalweb/portal/util/liferayselenium/LiferaySeleniumHelper.java
@@ -1251,9 +1251,7 @@ public class LiferaySeleniumHelper {
 
 		liferaySelenium.typeKeys(locator, "");
 
-		Pattern pattern = Pattern.compile("\\(|\\$\\{line\\.separator\\}");
-
-		Matcher matcher = pattern.matcher(value);
+		Matcher matcher = _aceEditorPattern.matcher(value);
 
 		int x = 0;
 
@@ -1624,6 +1622,8 @@ public class LiferaySeleniumHelper {
 		}
 	}
 
+	private static Pattern _aceEditorPattern = Pattern.compile(
+		"\\(|\\$\\{line\\.separator\\}");
 	private static List<Exception> _javaScriptExceptions =
 		new ArrayList<Exception>();
 	private static List<Exception> _liferayExceptions =
