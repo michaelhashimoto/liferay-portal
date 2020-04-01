b20967965757a5fad3f02b87cb8a2dfe3f1106ef
Michael Hashimoto <michael.hashimoto@liferay.com>
2020-04-01 12:20:44 -0700

    LRCI-1129 Add new method to get the batch test suite property

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -733,6 +733,69 @@ public class JenkinsResultsParserUtil {
 		return new File(buildProperties.getProperty("base.repository.dir"));
 	}
 
+	public static String getBatchTestSuiteProperty(
+		Properties properties, String basePropertyName, String batchName,
+		String testSuiteName) {
+
+		String propertyValue = getProperty(
+			properties,
+			combine(
+				basePropertyName, "[", batchName, "]", "[", testSuiteName,
+				"]"));
+
+		if (propertyValue != null) {
+			return propertyValue;
+		}
+
+		String batchTestSuitePropertyNameRegex = combine(
+			basePropertyName, "\\[([^\\]]+)\\]\\[", testSuiteName + "\\]");
+
+		for (String propertyName : properties.stringPropertyNames()) {
+			if (!propertyName.matches(batchTestSuitePropertyNameRegex)) {
+				continue;
+			}
+
+			String batchNameRegex = propertyName.replaceAll(
+				batchTestSuitePropertyNameRegex, "$1");
+
+			batchNameRegex = batchNameRegex.replace("*", ".+");
+
+			if (!batchName.matches(batchNameRegex)) {
+				continue;
+			}
+
+			return getProperty(properties, propertyName);
+		}
+
+		propertyValue = getProperty(
+			properties, combine(basePropertyName, "[", batchName, "]"));
+
+		if (propertyValue != null) {
+			return propertyValue;
+		}
+
+		String batchPropertyNameRegex = basePropertyName + "\\[([^\\]]+)\\]";
+
+		for (String propertyName : properties.stringPropertyNames()) {
+			if (!propertyName.matches(batchPropertyNameRegex)) {
+				continue;
+			}
+
+			String batchNameRegex = propertyName.replaceAll(
+				batchPropertyNameRegex, "$1");
+
+			batchNameRegex = batchNameRegex.replace("*", ".+");
+
+			if (!batchName.matches(batchNameRegex)) {
+				continue;
+			}
+
+			return getProperty(properties, propertyName);
+		}
+
+		return getProperty(properties, basePropertyName);
+	}
+
 	public static String getBuildParameter(String buildURL, String key) {
 		Map<String, String> buildParameters = getBuildParameters(buildURL);
 
