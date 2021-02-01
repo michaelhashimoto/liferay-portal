181849d36885f460466e6915cb2938c35e1ebce9
Michael Hashimoto <michael.hashimoto@liferay.com>
2021-02-01 14:04:37 -0800

    LRCI-1931 failures

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtilTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtilTest.java
@@ -57,6 +57,8 @@ public class JenkinsResultsParserUtilTest
 				"cloud-10-50-0-2,cloud-10-50-0-49,cloud-10-50-0-50",
 			JenkinsResultsParserUtil.expandSlaveRange(
 				"cloud-10-50-0-47, cloud-10-50-0-0..2, cloud-10-50-0-49..50"));
+
+		testEquals("one", "two");
 	}
 
 	@Test
