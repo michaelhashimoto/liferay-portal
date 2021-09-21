18f5a1b4ae58e5794c41caf8a382b43a5b53bd63
Michael Hashimoto <michael.hashimoto@liferay.com>
2021-09-21 12:47:22 -0700

    LRCI-2514 back if there is nothing

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitHubDevSyncUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/GitHubDevSyncUtil.java
@@ -1509,6 +1509,20 @@ public class GitHubDevSyncUtil {
 		RemoteGitBranch cacheRemoteGitBranch = fetchCacheBranchFromGitHubDev(
 			gitWorkingDirectory, cacheBranchName);
 
+		if (cacheRemoteGitBranch == null) {
+			return gitWorkingDirectory.getRebasedLocalGitBranch(
+				JenkinsResultsParserUtil.combine(
+					gitWorkingDirectory.getUpstreamBranchName(), "-temp-",
+					String.valueOf(
+						JenkinsResultsParserUtil.getCurrentTimeMillis())),
+				senderBranchName,
+				JenkinsResultsParserUtil.combine(
+					"git@github.com:", senderUsername, "/",
+					localGitRepository.getName()),
+				senderBranchSHA, gitWorkingDirectory.getUpstreamBranchName(),
+				upstreamBranchSHA);
+		}
+
 		return gitWorkingDirectory.fetch(
 			cacheLocalGitBranch, cacheRemoteGitBranch);
 	}
