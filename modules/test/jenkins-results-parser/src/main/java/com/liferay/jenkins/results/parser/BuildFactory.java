5118bea9db9b1df6265d564f71e51258bb6a1e8a
Peter Yoo <peter.yoo@liferay.com>
2021-12-16 12:19:31 -0800

    Revert "LRCI-2629 Add a buildURL pattern for local archive URLs"
    This reverts commit 93f8001c36ee4c7bb763e92d40e202749580be56.

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BuildFactory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BuildFactory.java
@@ -213,9 +213,6 @@ public class BuildFactory {
 			_BUILD_URL_SUFFIX_REGEX),
 		JenkinsResultsParserUtil.combine(
 			".*?Test/+[^/]+/+(?<master>test-[0-9]-[0-9]{1,2})/",
-			"(?<jobName>[^/]+)/?", _BUILD_URL_SUFFIX_REGEX),
-		JenkinsResultsParserUtil.combine(
-			"file:/.*", "(?<master>test-[0-9]-[0-9]{1,2})/",
 			"(?<jobName>[^/]+)/?", _BUILD_URL_SUFFIX_REGEX));
 
 }
