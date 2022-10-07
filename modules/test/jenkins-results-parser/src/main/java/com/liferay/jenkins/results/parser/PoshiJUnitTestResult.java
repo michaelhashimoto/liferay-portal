c477ca197fdb14991f3f0e566a33804e762e3752
Michael Hashimoto <michael.hashimoto@liferay.com>
2022-10-07 15:25:50 -0700

    LRCI-3231 Use test history in the GitHub message

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PoshiJUnitTestResult.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/PoshiJUnitTestResult.java
@@ -56,6 +56,22 @@ public class PoshiJUnitTestResult extends JUnitTestResult {
 			Dom4JUtil.getNewAnchorElement(
 				getConsoleOutputURL(), "Console Output"));
 
+		TestHistory testHistory = getTestHistory();
+
+		if (testHistory != null) {
+			downstreamBuildListItemElement.add(
+				Dom4JUtil.getNewElement("span", null, "-"));
+
+			downstreamBuildListItemElement.add(
+				Dom4JUtil.getNewAnchorElement(
+					testHistory.getTestrayCaseResultURL(),
+					JenkinsResultsParserUtil.combine(
+						"Failed ",
+						String.valueOf(testHistory.getFailureCount()),
+						" of last ",
+						String.valueOf(testHistory.getTestCount()))));
+		}
+
 		String errorDetails = getErrorDetails();
 
 		if ((errorDetails != null) && !errorDetails.isEmpty()) {
