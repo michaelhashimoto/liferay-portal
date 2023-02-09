e8b3e1b2daef6e0d84dbae12761d7208ad13779b
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-02-08 17:49:18 -0800

    LRCI-3406 Add CORS configuration for Spring Boot Security

---
+++ workspaces/liferay-jethr0-workspace/client-extensions/ce02-oauth-application-headless-server/src/main/java/com/liferay/jethr0/CE02EnableWebSecurity.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jethr0;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+/**
+ * @author Michael Hashimoto
+ */
+@Configuration
+@EnableWebSecurity
+public class CE02EnableWebSecurity {
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource =
+			new UrlBasedCorsConfigurationSource();
+
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+
+		corsConfiguration.setAllowedHeaders(
+			Arrays.asList("Authorization", "Content-Type"));
+		corsConfiguration.setAllowedMethods(
+			Arrays.asList(
+				"DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"));
+		corsConfiguration.setAllowedOrigins(_getLiferayAllowedOrigins());
+
+		urlBasedCorsConfigurationSource.registerCorsConfiguration(
+			"/**", corsConfiguration);
+
+		return urlBasedCorsConfigurationSource;
+	}
+
+	private List<String> _getLiferayAllowedOrigins() {
+		List<String> liferayAllowedOrigins = new ArrayList<>();
+
+		for (String dxpDomain : _liferayPortalDomains.split("\\s*[,\n]\\s*")) {
+			liferayAllowedOrigins.add("http://" + dxpDomain);
+			liferayAllowedOrigins.add("https://" + dxpDomain);
+		}
+
+		return liferayAllowedOrigins;
+	}
+
+	@Value("${liferay.portal.domains}")
+	private String _liferayPortalDomains;
+
+}
