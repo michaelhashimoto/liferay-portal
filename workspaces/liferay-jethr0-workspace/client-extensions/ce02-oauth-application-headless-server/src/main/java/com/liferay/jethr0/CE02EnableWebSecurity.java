a3be4cb264bd47c44fc5d6bed52226612b1db63d
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-02-08 18:32:55 -0800

    LRCI-3406 Add Jwt Decoder configuration for Spring Boot Security

--- workspaces/liferay-jethr0-workspace/client-extensions/ce02-oauth-application-headless-server/src/main/java/com/liferay/jethr0/CE02EnableWebSecurity.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/ce02-oauth-application-headless-server/src/main/java/com/liferay/jethr0/CE02EnableWebSecurity.java
@@ -14,9 +14,23 @@
 
 package com.liferay.jethr0;
 
+import com.nimbusds.jose.JOSEObjectType;
+import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
+import com.nimbusds.jose.proc.JWSAlgorithmFamilyJWSKeySelector;
+import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.proc.DefaultJWTProcessor;
+
+import java.net.URL;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -25,10 +39,17 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author Michael Hashimoto
@@ -55,6 +76,32 @@ public class CE02EnableWebSecurity {
 			"/**", corsConfiguration);
 
 		return urlBasedCorsConfigurationSource;
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder() throws Exception {
+		DefaultJWTProcessor<SecurityContext> defaultJWTProcessor =
+			new DefaultJWTProcessor<>();
+
+		defaultJWTProcessor.setJWSKeySelector(
+			JWSAlgorithmFamilyJWSKeySelector.fromJWKSetURL(
+				new URL(_liferayPortalURL + "/o/oauth2/jwks")));
+		defaultJWTProcessor.setJWSTypeVerifier(
+			new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType("at+jwt")));
+
+		NimbusJwtDecoder nimbusJwtDecoder = new NimbusJwtDecoder(
+			defaultJWTProcessor);
+
+		String liferayOAuthClientID = _getLiferayOAuthClientID();
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Using Liferay OAuth Client ID " + liferayOAuthClientID);
+		}
+
+		nimbusJwtDecoder.setJwtValidator(
+			new LiferayOAuth2TokenValidator(liferayOAuthClientID));
+
+		return nimbusJwtDecoder;
 	}
 
 	@Bean
@@ -91,7 +138,82 @@ public class CE02EnableWebSecurity {
 		return liferayAllowedOrigins;
 	}
 
+	private String _getLiferayOAuthClientID() throws Exception {
+		if (_liferayOAuthClientID != null) {
+			return _liferayOAuthClientID;
+		}
+
+		while (true) {
+			try {
+				StringBuilder sb = new StringBuilder();
+
+				sb.append(_liferayPortalURL);
+				sb.append("/o/oauth2/application?externalReferenceCode=");
+				sb.append(_liferayOAuthApplicationExternalReferenceCode);
+
+				String response = WebClient.builder(
+				).build(
+				).get(
+				).uri(
+					sb.toString()
+				).retrieve(
+				).bodyToMono(
+					String.class
+				).block();
+
+				JSONObject jsonObject = new JSONObject(response);
+
+				_liferayOAuthClientID = jsonObject.getString("client_id");
+
+				return _liferayOAuthClientID;
+			}
+			catch (Throwable throwable) {
+				if (_log.isWarnEnabled()) {
+					_log.warn(
+						"Unable to get client ID: " + throwable.getMessage());
+				}
+
+				Thread.sleep(1000);
+			}
+		}
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		CE02EnableWebSecurity.class);
+
+	@Value("${liferay.oauth.application.external.reference.code}")
+	private String _liferayOAuthApplicationExternalReferenceCode;
+
+	private String _liferayOAuthClientID;
+
 	@Value("${liferay.portal.domains}")
 	private String _liferayPortalDomains;
+
+	@Value("${liferay.portal.url}")
+	private String _liferayPortalURL;
+
+	private class LiferayOAuth2TokenValidator
+		implements OAuth2TokenValidator<Jwt> {
+
+		public LiferayOAuth2TokenValidator(String liferayOAuthClientID) {
+			_liferayOAuthClientID = liferayOAuthClientID;
+		}
+
+		@Override
+		public OAuth2TokenValidatorResult validate(Jwt jwt) {
+			if (Objects.equals(
+					jwt.getClaimAsString("client_id"), _liferayOAuthClientID)) {
+
+				return OAuth2TokenValidatorResult.success();
+			}
+
+			return OAuth2TokenValidatorResult.failure(_oAuth2Error);
+		}
+
+		private final String _liferayOAuthClientID;
+		private final OAuth2Error _oAuth2Error = new OAuth2Error(
+			"invalid_token", "The client_id does not match", null);
+
+	}
 
 }
