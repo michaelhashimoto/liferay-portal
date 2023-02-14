488512a9151bda584a495da80d639252a0672a0c
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-02-13 18:27:22 -0800

    LRCI-3407 Create one example request against the liferay server

--- workspaces/liferay-jethr0-workspace/client-extensions/ce02-oauth-application-headless-server/src/main/java/com/liferay/jethr0/CE02RestController.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/ce02-oauth-application-headless-server/src/main/java/com/liferay/jethr0/CE02RestController.java
@@ -14,13 +14,18 @@
 
 package com.liferay.jethr0;
 
+import com.liferay.jethr0.server.LiferayServer;
+import com.liferay.jethr0.server.Server;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,10 +36,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class CE02RestController {
 
 	@GetMapping("/")
-	public ResponseEntity<String> home() {
-		JSONObject jsonObject = new JSONObject();
-
-		jsonObject.put("name", "value");
+	public ResponseEntity<String> home(Jwt jwt) {
+		JSONObject jsonObject = new JSONObject(
+			_liferayServer.httpRequest(
+				"/o/c/tasks", null, jwt, Server.Method.GET, null, null));
 
 		if (_log.isInfoEnabled()) {
 			_log.info("Return: " + jsonObject);
@@ -44,5 +49,8 @@ public class CE02RestController {
 	}
 
 	private static final Log _log = LogFactory.getLog(CE02RestController.class);
+
+	@Autowired
+	private LiferayServer _liferayServer;
 
 }
