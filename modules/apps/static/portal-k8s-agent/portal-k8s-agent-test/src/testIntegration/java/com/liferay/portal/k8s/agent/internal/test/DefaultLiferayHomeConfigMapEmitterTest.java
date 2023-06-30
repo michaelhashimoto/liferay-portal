8f91654e382bb93da9134c64c862346ca844f9db
Gregory Amerson <gregory.amerson@liferay.com>
2023-06-30 17:26:11 -0500

    LPS-187069 add tests

---
+++ modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/DefaultLiferayHomeConfigMapEmitterTest.java
@@ -0,0 +1,234 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.k8s.agent.internal.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.k8s.agent.PortalK8sConfigMapModifier;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.SynchronousMailTestRule;
+
+import java.io.File;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import java.util.Arrays;
+import java.util.Map;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Gregory Amerson
+ */
+@RunWith(Arquillian.class)
+public class DefaultLiferayHomeConfigMapEmitterTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(), SynchronousMailTestRule.INSTANCE);
+
+	@BeforeClass
+	public static void setUpClass() throws Exception {
+		_bundle = FrameworkUtil.getBundle(
+			DefaultLiferayHomeConfigMapEmitterTest.class);
+
+		_bundleContext = _bundle.getBundleContext();
+
+		_liferayHomePath = Paths.get(System.getProperty("liferay.home"));
+
+		Assert.assertTrue(
+			"liferay.home does not exist", Files.exists(_liferayHomePath));
+
+		_serviceTracker = new ServiceTracker<>(
+			_bundleContext, PortalK8sConfigMapModifier.class, null);
+
+		_serviceTracker.open();
+
+		_portalK8sConfigMapModifier = _serviceTracker.waitForService(2000);
+
+		Assert.assertNotNull(_portalK8sConfigMapModifier);
+
+		ServiceReference<PortalK8sConfigMapModifier> serviceReference =
+			_serviceTracker.getServiceReference();
+
+		Assert.assertNull(serviceReference.getProperty("service.ranking"));
+	}
+
+	@AfterClass
+	public static void tearDownClass() throws Exception {
+		_serviceTracker.close();
+	}
+
+	@Test
+	public void testDefaultDxpMetadata() throws Exception {
+		Path dxpMetadataPath = _liferayHomePath.resolve(
+			"cx-metadata/" + TestPropsValues.COMPANY_WEB_ID + "/dxp-metadata");
+
+		Assert.assertTrue(
+			dxpMetadataPath.toString() + " does not exist",
+			Files.exists(dxpMetadataPath));
+
+		File dxpMetadataDir = dxpMetadataPath.toFile();
+
+		String[] files = dxpMetadataDir.list();
+
+		Assert.assertEquals(Arrays.toString(files), 3, files.length);
+
+		Path mainDomainPath = dxpMetadataPath.resolve(
+			"com.liferay.lxc.dxp.mainDomain");
+
+		Assert.assertTrue(
+			mainDomainPath.toString() + " does not exist",
+			Files.exists(mainDomainPath));
+
+		Assert.assertEquals(
+			"localhost", new String(Files.readAllBytes(mainDomainPath)));
+	}
+
+	@Test
+	public void testProjectMetadata() throws Exception {
+		String projectId = RandomTestUtil.randomString();
+		String serviceId = RandomTestUtil.randomString();
+
+		String configMapName = StringBundler.concat(
+			projectId, StringPool.DASH, TestPropsValues.COMPANY_WEB_ID,
+			"-lxc-ext-init-metadata");
+
+		_portalK8sConfigMapModifier.modifyConfigMap(
+			configMapModel -> {
+				Map<String, String> data = configMapModel.data();
+
+				data.put("foo", "bar");
+				data.put("fizz", "buzz");
+
+				Map<String, String> labels = configMapModel.labels();
+
+				labels.put("lxc.liferay.com/metadataType", "ext-init");
+				labels.put("ext.lxc.liferay.com/projectId", projectId);
+				labels.put("ext.lxc.liferay.com/serviceId", serviceId);
+				labels.put(
+					"dxp.lxc.liferay.com/virtualInstanceId",
+					TestPropsValues.COMPANY_WEB_ID);
+			},
+			configMapName);
+
+		Path projectMetadataPath = _liferayHomePath.resolve(
+			Paths.get(
+				"cx-metadata", TestPropsValues.COMPANY_WEB_ID, projectId));
+
+		Path dxpMetadataPath = projectMetadataPath.resolve("dxp-metadata");
+
+		Assert.assertTrue(
+			dxpMetadataPath.toString() + " should exist",
+			Files.exists(dxpMetadataPath));
+
+		Path extInitMetadataPath = projectMetadataPath.resolve(
+			"ext-init-metadata");
+
+		Assert.assertTrue(
+			extInitMetadataPath.toString() + " should exist",
+			Files.exists(extInitMetadataPath));
+
+		File extInitMetadataDir = extInitMetadataPath.toFile();
+
+		String[] files = extInitMetadataDir.list();
+
+		Assert.assertEquals(Arrays.toString(files), 2, files.length);
+
+		Path fooPath = extInitMetadataPath.resolve("foo");
+
+		Assert.assertTrue(
+			fooPath.toString() + " does not exist", Files.exists(fooPath));
+
+		Assert.assertEquals("bar", new String(Files.readAllBytes(fooPath)));
+
+		Path fizzPath = extInitMetadataPath.resolve("fizz");
+
+		Assert.assertTrue(
+			fizzPath.toString() + " does not exist", Files.exists(fooPath));
+
+		Assert.assertEquals("buzz", new String(Files.readAllBytes(fizzPath)));
+	}
+
+	@Test
+	public void testVirtualInstanceDxpMetadata() throws Exception {
+		String webId = "foo.lxc.com";
+
+		_companyLocalService.addCompany(
+			null, webId, webId, webId, 0, true, null, null, null, null, null,
+			null);
+
+		Path dxpMetadataPath = _liferayHomePath.resolve(
+			"cx-metadata/" + webId + "/dxp-metadata");
+
+		Assert.assertTrue(
+			dxpMetadataPath.toString() + " does not exist",
+			Files.exists(dxpMetadataPath));
+
+		File dxpMetadataDir = dxpMetadataPath.toFile();
+
+		String[] files = dxpMetadataDir.list();
+
+		Assert.assertEquals(Arrays.toString(files), 3, files.length);
+
+		Path mainDomainPath = dxpMetadataPath.resolve(
+			"com.liferay.lxc.dxp.mainDomain");
+
+		Assert.assertTrue(
+			mainDomainPath.toString() + " does not exist",
+			Files.exists(mainDomainPath));
+
+		Assert.assertEquals(
+			webId, new String(Files.readAllBytes(mainDomainPath)));
+	}
+
+	private static Bundle _bundle;
+	private static BundleContext _bundleContext;
+
+	@Inject
+	private static CompanyLocalService _companyLocalService;
+
+	private static Path _liferayHomePath;
+
+	@Inject
+	private static PortalK8sConfigMapModifier _portalK8sConfigMapModifier;
+
+	private static ServiceTracker
+		<PortalK8sConfigMapModifier, PortalK8sConfigMapModifier>
+			_serviceTracker;
+
+}
--- modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/PortalK8sAgentImplTest.java
+++ modules/apps/static/portal-k8s-agent/portal-k8s-agent-test/src/testIntegration/java/com/liferay/portal/k8s/agent/internal/test/PortalK8sAgentImplTest.java
@@ -78,6 +78,7 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.Constants;
 import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceReference;
 import org.osgi.service.cm.Configuration;
 import org.osgi.service.cm.ConfigurationAdmin;
 import org.osgi.util.tracker.ServiceTracker;
@@ -169,6 +170,12 @@ public class PortalK8sAgentImplTest {
 		_portalK8sConfigMapModifier = _serviceTracker.waitForService(2000);
 
 		Assert.assertNotNull(_portalK8sConfigMapModifier);
+
+		ServiceReference<PortalK8sConfigMapModifier> serviceReference =
+			_serviceTracker.getServiceReference();
+
+		Assert.assertEquals(
+			Integer.MAX_VALUE, serviceReference.getProperty("service.ranking"));
 	}
 
 	@AfterClass
