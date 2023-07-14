9dd750df8f54e680a1988bdea4edda73b0e292a7
Gregory Amerson <gregory.amerson@liferay.com>
2023-07-14 15:40:00 -0500

    LPS-187069 sf

--- workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-node/config.js
+++ workspaces/liferay-sample-workspace/client-extensions/liferay-sample-etc-node/config.js
@@ -18,7 +18,7 @@ export default {
 	'com.liferay.lxc.dxp.server.protocol': 'http',
 	'configTreePaths': [
 		process.env.DXP_METADATA_PATH,
-		process.env.EXT_INIT_METADATA_PATH
+		process.env.EXT_INIT_METADATA_PATH,
 	],
 	'liferay.oauth.application.external.reference.codes':
 		'liferay-sample-node-oauth-application-user-agent',
