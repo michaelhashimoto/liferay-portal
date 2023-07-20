af9ccfcf614a6696504967736f98d80178670be9
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-07-20 11:37:51 -0700

    LRCI-3706 Use instant instead of date format to get a date object

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/util/StringUtil.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/util/StringUtil.java
@@ -17,16 +17,14 @@ package com.liferay.jethr0.util;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
+
+import java.time.Instant;
 
 import java.util.Collection;
 import java.util.Date;
 import java.util.Locale;
 import java.util.Objects;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
 /**
  * @author Michael Hashimoto
@@ -94,19 +92,9 @@ public class StringUtil {
 			return null;
 		}
 
-		try {
-			return _simpleDateFormat.parse(dateString);
-		}
-		catch (ParseException parseException) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(
-					combine(
-						"Unable to parse date string \'", dateString, "\'\n",
-						parseException.getMessage()));
-			}
+		Instant instant = Instant.parse(dateString);
 
-			return null;
-		}
+		return new Date(instant.toEpochMilli());
 	}
 
 	public static String toLowerCase(String s) {
@@ -166,8 +154,6 @@ public class StringUtil {
 			throw new RuntimeException(malformedURLException);
 		}
 	}
-
-	private static final Log _log = LogFactory.getLog(StringUtil.class);
 
 	private static final SimpleDateFormat _simpleDateFormat =
 		new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
