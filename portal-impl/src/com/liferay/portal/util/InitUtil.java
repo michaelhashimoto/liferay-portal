191cc1dee85280f403402a1e219530e3652a8d20
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-09-06 11:52:05 -0700

    LPS-191780 Add debugging to InitUtil

--- portal-impl/src/com/liferay/portal/util/InitUtil.java
+++ portal-impl/src/com/liferay/portal/util/InitUtil.java
@@ -113,6 +113,10 @@ public class InitUtil {
 
 		ClassLoader classLoader = currentThread.getContextClassLoader();
 
+		System.out.println("-------------------------------------------------");
+		System.out.println("InitUtil#init");
+		System.out.println("classLoader=" + classLoader);
+
 		try {
 			PortalClassLoaderUtil.setClassLoader(classLoader);
 		}
