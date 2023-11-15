09f710021ff9a502210d5b7cbe3c654aca273eda
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-11-15 12:52:23 -0800

    LRCI-3875 Add logic to job repository to archive jobs older than 7 days

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/job/repository/JobEntityRepository.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/job/repository/JobEntityRepository.java
@@ -16,17 +16,26 @@ import com.liferay.jethr0.util.StringUtil;
 
 import java.util.Collections;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 /**
  * @author Michael Hashimoto
  */
 @Configuration
+@EnableScheduling
 public class JobEntityRepository extends BaseEntityRepository<JobEntity> {
 
 	public JobEntity create(
@@ -73,6 +82,9 @@ public class JobEntityRepository extends BaseEntityRepository<JobEntity> {
 
 	@Override
 	public void initialize() {
+		Date date = new Date(System.currentTimeMillis() - _getJobArchiveAge());
+
+		addAll(_jobEntityDALO.getAllAfterModifiedDate(date));
 	}
 
 	@Override
@@ -103,6 +115,33 @@ public class JobEntityRepository extends BaseEntityRepository<JobEntity> {
 		super.remove(jobEntities);
 	}
 
+	@Scheduled(cron = "${liferay.jethr0.job.archive.cron}")
+	public void scheduledArchive() {
+		Date date = new Date(System.currentTimeMillis() - _getJobArchiveAge());
+
+		Set<Long> jobEntityIds = new HashSet<>();
+
+		for (JobEntity jobEntity : getAll()) {
+			if (date.before(jobEntity.getModifiedDate()) ||
+				(jobEntity.getState() != JobEntity.State.COMPLETED)) {
+
+				continue;
+			}
+
+			jobEntityIds.add(jobEntity.getId());
+		}
+
+		Map<Long, JobEntity> entitiesMap = getEntitiesMap();
+
+		for (Long jobEntityId : jobEntityIds) {
+			entitiesMap.remove(jobEntityId);
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Archived " + jobEntityIds.size() + " jobs");
+		}
+	}
+
 	public void setBuildEntityRepository(
 		BuildEntityRepository buildEntityRepository) {
 
@@ -125,6 +164,10 @@ public class JobEntityRepository extends BaseEntityRepository<JobEntity> {
 		return jobEntity;
 	}
 
+	private long _getJobArchiveAge() {
+		return Long.getLong(_jobArchiveAgeInDays) * 1000 * 60 * 60 * 24;
+	}
+
 	private JobEntity _updateJobToBuildsRelationshipsFromDALO(
 		JobEntity parentJobEntity) {
 
@@ -138,7 +181,13 @@ public class JobEntityRepository extends BaseEntityRepository<JobEntity> {
 				buildEntity));
 	}
 
+	private static final Log _log = LogFactory.getLog(
+		JobEntityRepository.class);
+
 	private BuildEntityRepository _buildEntityRepository;
+
+	@Value("${JETHR0_JOB_ARCHIVE_AGE_IN_DAYS:7}")
+	private String _jobArchiveAgeInDays;
 
 	@Autowired
 	private JobEntityDALO _jobEntityDALO;
