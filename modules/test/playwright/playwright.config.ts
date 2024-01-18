91f5806228d2ffd8620d06899cf829816ac1bb05
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-01-18 14:21:23 -0800

    LRCI-4031 Remove batch-planner from playwright.config.ts

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -6,7 +6,6 @@
 import {defineConfig} from '@playwright/test';
 
 import {config as exportImportWeb} from './tests/export-import-web/config';
-import {config as batchPlanner} from './tests/batch-planner/config';
 import {config as setup} from './tests/global.setup.config';
 import {config as object} from './tests/object-web/config';
 import {config as portalWeb} from './tests/portal-web/config';
@@ -14,7 +13,7 @@ import {config as usersAdminWeb} from './tests/users-admin-web/config';
 
 export default defineConfig({
 	forbidOnly: !!process.env.CI,
-	projects: [batchPlanner, exportImportWeb, object, portalWeb, setup, usersAdminWeb],
+	projects: [exportImportWeb, object, portalWeb, setup, usersAdminWeb],
 	reporter: [
 		[
 			'html',
