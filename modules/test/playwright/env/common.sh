1613db3f3075fab8146805df39b97f1c27261b31
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-01-22 14:11:18 -0800

    LRCI-4031 Move all logic to deploy osgi & client extension

--- modules/test/playwright/env/common.sh
+++ modules/test/playwright/env/common.sh
@@ -39,42 +39,17 @@ function copy_to_deploy_folder() {
 function deploy_client_extensions() {
 	mkdir -p ${LIFERAY_HOME}/deploy
 
-	for client_extension_dir in $@
-	do
-		if [[ ! -d ${PROJECT_DIR}/${client_extension_dir} ]]
-		then
-			echo "${PROJECT_DIR}/${client_extension_dir} does not exist."
+	cd ${PROJECT_DIR}
 
-			exit 1
-		fi
-
-		cd ${PROJECT_DIR}/${client_extension_dir}
-
-		if [[ -e ${PROJECT_DIR}/${client_extension_dir}/client-extension.poshi.yaml ]]
-		then
-			${PROJECT_DIR}/gradlew clean deployPoshi -Pliferay.workspace.home.dir=${LIFERAY_HOME}
-		else
-			${PROJECT_DIR}/gradlew clean deploy -Pliferay.workspace.home.dir=${LIFERAY_HOME}
-		fi
-	done
+	ant -f build-test-playwright.xml deploy-client-extensions -Dclient.extension.dirs="${@}"
 }
 
 function deploy_osgi_modules() {
 	mkdir -p ${LIFERAY_HOME}/deploy
 
-	for osgi_module_dir in $@
-	do
-		if [[ ! -d ${PROJECT_DIR}/${osgi_module_dir} ]]
-		then
-			echo "${PROJECT_DIR}/${osgi_module_dir} does not exist."
+	cd ${PROJECT_DIR}
 
-			exit 1
-		fi
-
-		cd ${PROJECT_DIR}/${osgi_module_dir}
-
-		${PROJECT_DIR}/gradlew clean deploy
-	done
+	ant -f build-test-playwright.xml deploy-osgi-modules -Dosgi.module.dirs="${@}"
 }
 
 function deploy_project_client_extensions() {
