6931000b9bc64b784b2c3fc3d4cd3968a0d5bc2b
Marcos Martins <marcos.martins@liferay.com>
2024-02-02 10:36:17 -0300

    LPD-16508 login in Analytics Cloud

---
+++ modules/test/playwright/fixtures/loginAnalyticsCloudTest.ts
@@ -0,0 +1,65 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import {expect, test} from '@playwright/test';
+
+import createTempFile, {readTempFile} from '../utils/createTempFile';
+import {faroConfig} from '../tests/osb-faro-web/faro.config';
+import { Login } from './loginTest';
+
+let loggedIn = false;
+
+const loginAnalyticsCloudTest = test.extend<{
+	loginAnalyticsCloud: Login;
+}>({
+	loginAnalyticsCloud: [
+		async ({page}, use) => {
+			const user = faroConfig.user.login;
+			const password = faroConfig.user.password;
+
+			if (!loggedIn) {
+				const storageStatePath = createTempFile('analyticsCloudStorageState.json');
+
+				await page.goto(faroConfig.environment.baseUrl);
+
+				await page.getByRole('button', {name: 'Sign In'}).click();
+
+				await page.getByLabel('Email Address').fill(user);
+				await page.getByLabel('Password').fill(password);
+				await page.getByLabel('Remember Me').check();
+
+				await page
+					.getByRole('button', {name: 'Sign In'})
+					.click();
+
+				await expect(page.getByText('Your Workspaces')).toBeVisible({
+					timeout: 100 * 1000,
+				});
+
+				await page.context().storageState({path: storageStatePath});
+
+				loggedIn = true;
+			}
+			else {
+				const {cookies} = JSON.parse(readTempFile('analyticsCloudStorageState.json'));
+
+				page.context().addCookies(cookies);
+			}
+
+			const cookies = await page.context().cookies();
+
+			await use({
+				password,
+				sessionId: cookies.find(
+					(cookie) => cookie.name === 'JSESSIONID'
+				).value,
+				user,
+			});
+		},
+		{auto: true},
+	],
+});
+
+export {loginAnalyticsCloudTest};
