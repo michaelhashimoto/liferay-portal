08314699fb7bf80f5aa6d12b2a19ec63493c6498
Marcos Martins <marcos.martins@liferay.com>
2024-02-02 14:59:51 -0300

    TEMP set environment variables

--- modules/test/playwright/tests/analytics-settings-web/setup.sh
+++ modules/test/playwright/tests/analytics-settings-web/setup.sh
@@ -2,6 +2,10 @@
 
 set -e -x
 
+export FARO_URL=http://localhost:8081
+
+export PORTAL_URL=http://"$(hostname  -I | cut -f1 -d' ')":8080
+
 source ${PLAYWRIGHT_BASE_DIR}/env/common.sh
 
 update_portal_ext_properties
