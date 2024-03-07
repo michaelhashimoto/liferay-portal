914ec0ad2ba862ca9d81330f26b78beceda03c3e
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-03-07 11:45:39 -0800

    LRCI-3969 Add logic to prevent running ci:test:forward

--- workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/github/BaseTestGitHubCommentEventHandler.java
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-etc-spring-boot/src/main/java/com/liferay/jethr0/event/github/BaseTestGitHubCommentEventHandler.java
@@ -7,8 +7,10 @@ package com.liferay.jethr0.event.github;
 
 import com.liferay.jethr0.event.EventHandlerContext;
 import com.liferay.jethr0.event.github.comment.GitHubComment;
+import com.liferay.jethr0.event.github.pullrequest.GitHubPullRequest;
 import com.liferay.jethr0.job.JobEntity;
 import com.liferay.jethr0.job.PullRequestJobEntity;
+import com.liferay.jethr0.util.StringUtil;
 
 import java.io.IOException;
 
@@ -18,6 +20,9 @@ import java.util.List;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.json.JSONObject;
 
@@ -30,7 +35,8 @@ public abstract class BaseTestGitHubCommentEventHandler
 	@Override
 	public String process() throws InvalidJSONException, IOException {
 		if (checkLiferayGitHubUser() ||
-			closeInvalidUpstreamGitHubBranchName()) {
+			closeInvalidUpstreamGitHubBranchName() ||
+			_checkInvalidForwardTestSuiteName()) {
 
 			return null;
 		}
@@ -89,6 +95,33 @@ public abstract class BaseTestGitHubCommentEventHandler
 
 		return "default";
 	}
+
+	private boolean _checkInvalidForwardTestSuiteName()
+		throws InvalidJSONException, IOException {
+
+		String testSuite = getTestSuite();
+
+		if (!testSuite.equals("forward")) {
+			return false;
+		}
+
+		GitHubPullRequest gitHubPullRequest = getGitHubPullRequest();
+
+		String message = StringUtil.combine(
+			"The test will not be initiated because `ci:test:forward` is not ",
+			"a valid command.  Please use `ci:forward` instead.");
+
+		gitHubPullRequest.comment(message);
+
+		if (_log.isInfoEnabled()) {
+			_log.info(message);
+		}
+
+		return true;
+	}
+
+	private static final Log _log = LogFactory.getLog(
+		BaseTestGitHubCommentEventHandler.class);
 
 	private static final Pattern _pattern = Pattern.compile(
 		"ci:test(\\:(?<testOptions>[^\\s]+))?");
