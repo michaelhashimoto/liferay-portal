05af9bd03b51815255c3ac1f479e73a3eeebd4cc
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-05-10 10:55:55 -0700

    LRCI-4226 Add basic objects for jenkins cohorts & servers in the UI

---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-custom-element/src/objects/jenkins/JenkinsServer.js
@@ -0,0 +1,16 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+export default class JenkinsServer {
+	constructor({dateCreated, dateModified, id, name, url}) {
+		this.dateCreated = dateCreated;
+		this.dateModified = dateModified;
+		this.id = id;
+		this.name = name;
+		this.url = url;
+	}
+
+	jenkinsCohort;
+}
---
+++ workspaces/liferay-jethr0-workspace/client-extensions/liferay-jethr0-custom-element/src/objects/jenkins/JenkinsUtil.js
@@ -0,0 +1,131 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+import liferayRequest from '../../services/liferayRequest';
+import JenkinsCohort from './JenkinsCohort';
+import JenkinsServer from './JenkinsServer';
+
+export async function deleteJenkinsCohortById({id, redirect}) {
+	const response = await liferayRequest({
+		method: 'DELETE',
+		urlPath: '/o/c/jenkins-cohort/' + id,
+	});
+
+	await response.text();
+
+	if (redirect) {
+		redirect(null);
+	}
+}
+
+export async function getJenkinsCohortById({id, setJenkinsCohort}) {
+	const response = await liferayRequest({
+		graphqlQuery: `{
+			c {
+				jenkinsCohorts(filter: \\"id eq '${id}'\\") {
+					items {
+						dateCreated
+						dateModified
+						id
+						name
+					}
+				}
+				jenkinsServers(filter: \\"r_jenkinsCohortToJenkinsServers_c_jenkinsCohortId eq '${id}'\\") {
+					items {
+						dateCreated
+						dateModified
+						id
+						jenkinsCohortToJenkinsServers
+						name
+						url
+					}
+				}
+			}
+		}`,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: 'POST',
+		urlPath: '/o/graphql',
+	});
+
+	const result = JSON.parse(await response.text());
+
+	const jenkinsServers = [];
+
+	for (const jenkinsServersJSON of result.data.c.jenkinsServers.items) {
+		jenkinsServers.push(new JenkinsServer(jenkinsServersJSON));
+	}
+
+	for (const jenkinsCohortJSON of result.data.c.jenkinsCohorts.items) {
+		const jenkinsCohort = new JenkinsCohort(jenkinsCohortJSON);
+
+		jenkinsCohort.jenkinsServers = jenkinsServers;
+
+		if (jenkinsCohort) {
+			if (setJenkinsCohort) {
+				setJenkinsCohort(jenkinsCohort);
+			}
+
+			return jenkinsCohort;
+		}
+	}
+}
+
+export async function getJenkinsCohortsPage({
+	page,
+	pageSize,
+	setJenkinsCohortsPage,
+}) {
+	if (!page) {
+		page = 1;
+	}
+
+	if (!pageSize) {
+		pageSize = 25;
+	}
+
+	const response = await liferayRequest({
+		graphqlQuery: `{
+			c {
+				jenkinsCohorts (page: ${page}, pageSize: ${pageSize}) {
+					items {
+						id
+						dateCreated
+						dateModified
+						name
+					}
+					page
+					pageSize
+					totalCount
+				}
+			}
+		}`,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		method: 'POST',
+		urlPath: '/o/graphql',
+	});
+
+	const result = JSON.parse(await response.text());
+
+	const jenkinsCohorts = [];
+
+	for (const item of result.data.c.jenkinsCohorts.items) {
+		jenkinsCohorts.push(new JenkinsCohort(item));
+	}
+
+	const jenkinsCohortsPage = {
+		jenkinsCohorts,
+		page: result.data.c.jenkinsCohorts.page,
+		pageSize: result.data.c.jenkinsCohorts.pageSize,
+		totalCount: result.data.c.jenkinsCohorts.totalCount,
+	};
+
+	if (setJenkinsCohortsPage) {
+		setJenkinsCohortsPage(jenkinsCohortsPage);
+	}
+}
