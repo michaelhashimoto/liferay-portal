9d730e0f821bfb00cd457d43d7f8f94bc381005f
Michael Hashimoto <michael.hashimoto@liferay.com>
2024-06-26 11:32:35 -0700

    LRCI-UNIT Add test

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil2Test.java
@@ -0,0 +1,20 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser;
+
+import org.junit.Test;
+
+/**
+ * @author Peter Yoo
+ */
+public class JenkinsResultsParserUtil2Test {
+
+	@Test
+	public void testOne() {
+		System.out.println("RUNNING TEST ONE");
+	}
+
+}
