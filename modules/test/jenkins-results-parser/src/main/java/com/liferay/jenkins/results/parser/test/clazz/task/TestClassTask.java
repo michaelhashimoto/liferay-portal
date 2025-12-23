4d7bcda5402fc3a3c4be60e370ad1e8642280c14
Michael Hashimoto <michael.hashimoto@liferay.com>
2025-12-23 09:36:13 -0800

    LRCI-6810 Add test class task objects

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/task/TestClassTask.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2025 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser.test.clazz.task;
+
+import com.liferay.jenkins.results.parser.test.clazz.TestClass;
+
+import java.util.List;
+
+import org.json.JSONObject;
+
+/**
+ * @author Michael Hashimoto
+ */
+public interface TestClassTask {
+
+	public void addTestClasses(TestClass testClass);
+
+	public long getAverageDuration();
+
+	public JSONObject getJSONObject();
+
+	public String getTaskName();
+
+	public List<TestClass> getTestClasses();
+
+}
