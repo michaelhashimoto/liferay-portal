05d42f63efc10ebfac63907ce851023be63544cf
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-03-20 08:01:51 -0700

    LRCI-7159 clean up

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/BaseBuild.java
@@ -3555,6 +3555,8 @@ public abstract class BaseBuild implements Build {
 		else {
 			setStatus("running");
 		}
+
+		saveBuildURLInBuildDatabase();
 	}
 
 	private void _setInvocationURL(String invocationURL) {
