6afe1e52281cedfe947f1a1fe8aeacc395690fc2
Iván Zaera Avellón <ivan.zaera@liferay.com>
2026-03-26 14:21:17 +0100

    LPD-83356 Handle fatal errors in workers
    Otherwise the SF may hang forever because it seems the workers never finish.

--- modules/frontend-sdk/node-scripts/util/format/util/formatSourceFilesWorker.mjs
+++ modules/frontend-sdk/node-scripts/util/format/util/formatSourceFilesWorker.mjs
@@ -3,11 +3,27 @@
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
+import print from '../../print.mjs';
 import formatSourceFile from './formatSourceFile.mjs';
 
 process.on('message', async ({fileIndex, filePath, options, skip}) => {
+	let result;
+
+	try {
+		result = await formatSourceFile(filePath, skip, options);
+	}
+	catch (error) {
+		print(
+			1,
+			print.error('ERROR:'),
+			`Worker failed formatting ${filePath}: ${error}`
+		);
+
+		result = false;
+	}
+
 	process.send({
 		fileIndex,
-		result: await formatSourceFile(filePath, skip, options),
+		result,
 	});
 });
