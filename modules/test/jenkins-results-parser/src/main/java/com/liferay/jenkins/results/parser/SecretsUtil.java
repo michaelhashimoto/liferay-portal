15b3dc68514779c4243603aed88522c50b67a139
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-04-16 07:34:50 -0700

    LRCI-7269 WORKING COPY

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SecretsUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/SecretsUtil.java
@@ -7,7 +7,6 @@ package com.liferay.jenkins.results.parser;
 
 import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil.BearerHTTPAuthorization;
 
-import java.io.File;
 import java.io.IOException;
 
 import java.util.ArrayList;
@@ -15,6 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.TimeoutException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -72,18 +72,24 @@ public abstract class SecretsUtil {
 			return null;
 		}
 
-		Field field = item.getField(fieldLabel);
+		ItemField itemField = item.getItemField(fieldLabel);
 
-		if (field == null) {
-			System.out.println(
-				JenkinsResultsParserUtil.combine(
-					"Field Not Found: ", vaultName, "/", itemTitle, "/",
-					fieldLabel));
-
-			return null;
+		if (itemField != null) {
+			return itemField.value;
 		}
 
-		return field.value;
+		ItemFile itemFile = item.getItemFile(fieldLabel);
+
+		if (itemFile != null) {
+			return itemFile.getValue();
+		}
+
+		System.out.println(
+			JenkinsResultsParserUtil.combine(
+				"Field Not Found: op://", vaultName, "/", itemTitle, "/",
+				fieldLabel));
+
+		return null;
 	}
 
 	public static boolean isSecretProperty(String value) {
@@ -96,6 +102,32 @@ public abstract class SecretsUtil {
 		return matcher.matches();
 	}
 
+	private static String _getOnePasswordConnectServerURL() {
+		if (_onePasswordConnectURL != null) {
+			return _onePasswordConnectURL;
+		}
+
+		String onePasswordConnectURL;
+
+		try {
+			onePasswordConnectURL = JenkinsResultsParserUtil.getBuildProperty(
+				"one.password.connect.url");
+
+			if (!JenkinsResultsParserUtil.isURL(onePasswordConnectURL)) {
+				onePasswordConnectURL = _SERVER_URL_DEFAULT;
+			}
+		}
+		catch (IOException ioException) {
+			System.out.println("WARNING: Please set one.password.connect.url");
+
+			onePasswordConnectURL = _SERVER_URL_DEFAULT;
+		}
+
+		_onePasswordConnectURL = onePasswordConnectURL;
+
+		return _onePasswordConnectURL;
+	}
+
 	private static JSONArray _toJSONArray(String path) {
 		if (_bearerHTTPAuthorization == null) {
 			return new JSONArray();
@@ -103,7 +135,8 @@ public abstract class SecretsUtil {
 
 		try {
 			return JenkinsResultsParserUtil.toJSONArray(
-				_SERVER_URL + path, null, _bearerHTTPAuthorization);
+				_getOnePasswordConnectServerURL() + path, null,
+				_bearerHTTPAuthorization);
 		}
 		catch (IOException ioException) {
 			System.out.println(ioException.getMessage());
@@ -121,7 +154,8 @@ public abstract class SecretsUtil {
 
 		try {
 			return JenkinsResultsParserUtil.toJSONObject(
-				_SERVER_URL + path, null, _bearerHTTPAuthorization);
+				_getOnePasswordConnectServerURL() + path, null,
+				_bearerHTTPAuthorization);
 		}
 		catch (IOException ioException) {
 			System.out.println(ioException.getMessage());
@@ -132,56 +166,81 @@ public abstract class SecretsUtil {
 		}
 	}
 
-	private static final String _SERVER_URL = "https://1password.liferay.com";
+	private static String _toString(String path) {
+		if (_bearerHTTPAuthorization == null) {
+			return "";
+		}
+
+		try {
+			return JenkinsResultsParserUtil.toString(
+				_getOnePasswordConnectServerURL() + path, null,
+				_bearerHTTPAuthorization);
+		}
+		catch (IOException ioException) {
+			System.out.println(ioException.getMessage());
+
+			ioException.printStackTrace();
+
+			return "";
+		}
+	}
+
+	private static final String _ACCESS_TOKEN;
+
+	private static final String _SERVER_URL_DEFAULT =
+		"https://1password.liferay.com";
 
 	private static final BearerHTTPAuthorization _bearerHTTPAuthorization;
 	private static final Pattern _keyPattern = Pattern.compile(
-		"(secret\\:)?(?<vaultName>[^\\/]*)\\/" +
+		"(op\\:\\/\\/|secret\\:)?(?<vaultName>[^\\/]*)\\/" +
 			"(?<itemTitle>[^\\/]*)\\/(?<fieldLabel>.*)");
+	private static String _onePasswordConnectURL;
 	private static final Pattern _secretPropertyPattern = Pattern.compile(
-		"secret\\:(?<key>.*)");
+		"(op\\:\\/\\/|secret\\:)(?<key>.*)");
 
 	static {
-		String token = null;
+		String accessToken = null;
 
 		try {
-			token = JenkinsResultsParserUtil.read(
-				new File(
-					System.getProperty("user.home") + "/.1password.connect"));
+			String onePasswordAccessTokenKey =
+				JenkinsResultsParserUtil.getBuildProperty(
+					"one.password.access.token.key");
 
-			token = token.trim();
+			if (JenkinsResultsParserUtil.isNullOrEmpty(
+					onePasswordAccessTokenKey)) {
+
+				System.out.println(
+					"WARNING: Please set one.password.access.token.key");
+			}
+
+			Process process = JenkinsResultsParserUtil.executeBashCommands(
+				"aws ssm get-parameter --name \"" + onePasswordAccessTokenKey +
+					"\" --with-decryption | jq -r .Parameter.Value");
+
+			accessToken = JenkinsResultsParserUtil.readInputStream(
+				process.getInputStream());
+
+			accessToken = accessToken.replace(
+				"Finished executing Bash commands.", "");
+
+			accessToken = accessToken.trim();
 		}
-		catch (IOException ioException) {
-			token = null;
-
+		catch (IOException | TimeoutException exception) {
 			System.out.println(
 				"Unable to load 1Password connect bearer token.");
 		}
 
-		if (token != null) {
-			JenkinsResultsParserUtil.addRedactToken(token);
+		_ACCESS_TOKEN = accessToken;
 
-			_bearerHTTPAuthorization = new BearerHTTPAuthorization(token);
+		if (accessToken != null) {
+			JenkinsResultsParserUtil.addRedactToken(_ACCESS_TOKEN);
+
+			_bearerHTTPAuthorization = new BearerHTTPAuthorization(
+				_ACCESS_TOKEN);
 		}
 		else {
 			_bearerHTTPAuthorization = null;
 		}
-	}
-
-	private static class Field {
-
-		public Field(String label, String value) {
-			this.label = label;
-			this.value = value;
-
-			if (!JenkinsResultsParserUtil.isNullOrEmpty(value)) {
-				JenkinsResultsParserUtil.addRedactToken(value);
-			}
-		}
-
-		public final String label;
-		public final String value;
-
 	}
 
 	private static class Item {
@@ -193,19 +252,37 @@ public abstract class SecretsUtil {
 			_vault = vault;
 		}
 
-		public Field getField(String label) {
-			if (_fields == null) {
+		public ItemField getItemField(String label) {
+			if (_itemFields == null) {
 				init();
 			}
 
-			for (Field field : _fields) {
-				if (Objects.equals(field.label, label)) {
-					return field;
+			for (ItemField itemField : _itemFields) {
+				if (Objects.equals(itemField.label, label)) {
+					return itemField;
 				}
 			}
 
 			if (_linkedItem != null) {
-				return _linkedItem.getField(label);
+				return _linkedItem.getItemField(label);
+			}
+
+			return null;
+		}
+
+		public ItemFile getItemFile(String fileName) {
+			if (_itemFiles == null) {
+				init();
+			}
+
+			for (ItemFile itemFile : _itemFiles) {
+				if (Objects.equals(itemFile.name, fileName)) {
+					return itemFile;
+				}
+			}
+
+			if (_linkedItem != null) {
+				return _linkedItem.getItemFile(fileName);
 			}
 
 			return null;
@@ -218,7 +295,7 @@ public abstract class SecretsUtil {
 
 			JSONArray fieldsJSONArray = itemJSONObject.getJSONArray("fields");
 
-			_fields = new ArrayList<>(fieldsJSONArray.length());
+			_itemFields = new ArrayList<>(fieldsJSONArray.length());
 
 			for (int i = 0; i < fieldsJSONArray.length(); i++) {
 				JSONObject fieldJSONObject = fieldsJSONArray.getJSONObject(i);
@@ -245,8 +322,8 @@ public abstract class SecretsUtil {
 						continue;
 					}
 
-					_fields.add(
-						new Field(
+					_itemFields.add(
+						new ItemField(
 							fieldJSONObject.getString("label"),
 							fieldJSONObject.getString("value")));
 				}
@@ -255,14 +332,104 @@ public abstract class SecretsUtil {
 					System.out.println(fieldJSONObject.toString(2));
 				}
 			}
+
+			JSONArray filesJSONArray = itemJSONObject.optJSONArray(
+				"files", new JSONArray());
+
+			_itemFiles = new ArrayList<>(filesJSONArray.length());
+
+			for (int i = 0; i < filesJSONArray.length(); i++) {
+				JSONObject fileJSONObject = filesJSONArray.getJSONObject(i);
+
+				try {
+					JSONObject sectionJSONObject = fileJSONObject.optJSONObject(
+						"section");
+
+					if (sectionJSONObject != null) {
+						if (Objects.equals(
+								sectionJSONObject.optString("label"),
+								"Related Items")) {
+
+							_linkedItem = _vault.getItem(
+								fileJSONObject.getString("label"));
+						}
+
+						if (_linkedItem != null) {
+							continue;
+						}
+					}
+
+					if (!fileJSONObject.has("content_path")) {
+						continue;
+					}
+
+					_itemFiles.add(
+						new ItemFile(
+							fileJSONObject.getString("content_path"),
+							fileJSONObject.getString("name")));
+				}
+				catch (JSONException jsonException) {
+					System.err.println(jsonException.toString());
+					System.out.println(fileJSONObject.toString(2));
+				}
+			}
 		}
 
 		public final String id;
 		public final String title;
 
-		private List<Field> _fields;
+		private List<ItemField> _itemFields;
+		private List<ItemFile> _itemFiles;
 		private Item _linkedItem;
 		private final Vault _vault;
+
+	}
+
+	private static class ItemField {
+
+		public ItemField(String label, String value) {
+			this.label = label;
+			this.value = value;
+
+			if (!JenkinsResultsParserUtil.isNullOrEmpty(value)) {
+				JenkinsResultsParserUtil.addRedactToken(value);
+			}
+		}
+
+		public final String label;
+		public final String value;
+
+	}
+
+	private static class ItemFile {
+
+		public ItemFile(String contentPath, String name) {
+			this.contentPath = contentPath;
+			this.name = name;
+		}
+
+		public String getValue() {
+			if (_value != null) {
+				return _value;
+			}
+
+			String value = _toString(contentPath);
+
+			value = value.trim();
+
+			if (!JenkinsResultsParserUtil.isNullOrEmpty(value)) {
+				JenkinsResultsParserUtil.addRedactToken(value);
+			}
+
+			_value = value;
+
+			return _value;
+		}
+
+		public final String contentPath;
+		public final String name;
+
+		private String _value;
 
 	}
 
@@ -278,7 +445,9 @@ public abstract class SecretsUtil {
 			}
 
 			for (Item item : _items) {
-				if (Objects.equals(item.title, title)) {
+				if (Objects.equals(item.id, title) ||
+					Objects.equals(item.title, title)) {
+
 					return item;
 				}
 			}
@@ -313,9 +482,7 @@ public abstract class SecretsUtil {
 		private static final Map<String, Vault> _vaultsMap = new HashMap<>();
 
 		static {
-			JSONArray vaultsJSONArray = new JSONArray();
-
-			//JSONArray vaultsJSONArray = _toJSONArray("/v1/vaults");
+			JSONArray vaultsJSONArray = _toJSONArray("/v1/vaults");
 
 			for (int i = 0; i < vaultsJSONArray.length(); i++) {
 				JSONObject vaultJSONObject = vaultsJSONArray.getJSONObject(i);
