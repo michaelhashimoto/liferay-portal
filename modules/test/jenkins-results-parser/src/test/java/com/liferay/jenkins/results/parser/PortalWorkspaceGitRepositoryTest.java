6bf53c47439689e717db86e2a2acafff34c03ca7
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-07-09 09:43:42 -0700

    LRCI-7783 Add tests locking the binaries cache toggle to only the binaries cache path

--- modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/PortalWorkspaceGitRepositoryTest.java
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/PortalWorkspaceGitRepositoryTest.java
@@ -7,9 +7,12 @@ package com.liferay.jenkins.results.parser;
 
 import java.io.File;
 
+import java.lang.reflect.Method;
+
 import java.util.Collections;
 import java.util.Properties;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import org.mockito.Mockito;
@@ -142,6 +145,85 @@ public class PortalWorkspaceGitRepositoryTest
 		);
 	}
 
+	@Test
+	public void testSetUpAdditionalCachesWithBinariesCacheDisabled()
+		throws Exception {
+
+		mockEnvironment(
+			Collections.singletonMap("MASTER_NETWORK_NAME", "aws-network"));
+
+		Properties buildProperties = new Properties();
+
+		buildProperties.setProperty("binaries.cache.enabled", "false");
+		buildProperties.setProperty("git.archive.enabled", "true");
+
+		JenkinsResultsParserUtil.setBuildProperties(buildProperties);
+
+		Shell shell = mockShell();
+
+		PortalWorkspaceGitRepository portalWorkspaceGitRepository =
+			_newPortalWorkspaceGitRepository();
+
+		portalWorkspaceGitRepository.setUpAdditionalCaches();
+
+		Mockito.verify(
+			shell, Mockito.never()
+		).doExecute(
+			Mockito.argThat(
+				executionRequest -> _isCommand(
+					executionRequest, "aws s3 cp", "binaries-cache.tar.gz"))
+		);
+
+		Assert.assertTrue(
+			"The git archive must stay enabled when binaries cache is disabled",
+			_isGitArchiveEnabled(portalWorkspaceGitRepository));
+	}
+
+	@Test
+	public void testSetUpAdditionalCachesWithBinariesCacheEnabled()
+		throws Exception {
+
+		mockEnvironment(
+			Collections.singletonMap("MASTER_NETWORK_NAME", "aws-network"));
+
+		Properties buildProperties = new Properties();
+
+		buildProperties.setProperty("binaries.cache.enabled", "true");
+		buildProperties.setProperty(
+			"binaries.cache.s3.path",
+			"s3://liferayci-file-propagator/binaries-cache/master.tar.gz");
+		buildProperties.setProperty("git.archive.enabled", "false");
+
+		JenkinsResultsParserUtil.setBuildProperties(buildProperties);
+
+		Shell shell = mockShell();
+
+		Mockito.doReturn(
+			new Shell.ExecutionResult(0, "", "")
+		).when(
+			shell
+		).doExecute(
+			Mockito.any(Shell.ExecutionRequest.class)
+		);
+
+		PortalWorkspaceGitRepository portalWorkspaceGitRepository =
+			_newPortalWorkspaceGitRepository();
+
+		portalWorkspaceGitRepository.setUpAdditionalCaches();
+
+		Mockito.verify(
+			shell
+		).doExecute(
+			Mockito.argThat(
+				executionRequest -> _isCommand(
+					executionRequest, "aws s3 cp", "binaries-cache.tar.gz"))
+		);
+
+		Assert.assertFalse(
+			"The git archive must stay disabled when binaries cache is enabled",
+			_isGitArchiveEnabled(portalWorkspaceGitRepository));
+	}
+
 	private boolean _isCommand(
 		Shell.ExecutionRequest executionRequest, String... substrings) {
 
@@ -158,6 +240,55 @@ public class PortalWorkspaceGitRepositoryTest
 		}
 
 		return true;
+	}
+
+	private boolean _isGitArchiveEnabled(
+			WorkspaceGitRepository workspaceGitRepository)
+		throws Exception {
+
+		Method method = BaseWorkspaceGitRepository.class.getDeclaredMethod(
+			"_isGitArchiveEnabled");
+
+		method.setAccessible(true);
+
+		return (Boolean)method.invoke(workspaceGitRepository);
+	}
+
+	private PortalWorkspaceGitRepository _newPortalWorkspaceGitRepository()
+		throws Exception {
+
+		File workingDirectory = File.createTempFile("portal-workspace-", null);
+
+		workingDirectory.delete();
+
+		workingDirectory.mkdir();
+
+		PortalWorkspaceGitRepository portalWorkspaceGitRepository =
+			Mockito.mock(PortalWorkspaceGitRepository.class);
+
+		Mockito.doReturn(
+			workingDirectory
+		).when(
+			portalWorkspaceGitRepository
+		).getDirectory();
+
+		Mockito.doReturn(
+			"master"
+		).when(
+			portalWorkspaceGitRepository
+		).getUpstreamBranchName();
+
+		Mockito.doCallRealMethod(
+		).when(
+			portalWorkspaceGitRepository
+		).isBinariesCacheEnabled();
+
+		Mockito.doCallRealMethod(
+		).when(
+			portalWorkspaceGitRepository
+		).setUpAdditionalCaches();
+
+		return portalWorkspaceGitRepository;
 	}
 
 }
