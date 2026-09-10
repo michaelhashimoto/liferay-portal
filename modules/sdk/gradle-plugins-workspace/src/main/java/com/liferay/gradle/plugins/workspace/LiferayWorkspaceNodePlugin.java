d8e6760b0ba22a3e0864ecc4d20babd04bfb44a5
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-09-09 18:41:32 -0700

    LRCI-8149 UPDATE

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/LiferayWorkspaceNodePlugin.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/LiferayWorkspaceNodePlugin.java
@@ -12,15 +12,12 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import com.liferay.gradle.plugins.node.NodeExtension;
 import com.liferay.gradle.plugins.node.NodePlugin;
-import com.liferay.gradle.plugins.node.task.PackageRunTestTask;
 import com.liferay.gradle.plugins.workspace.internal.util.GradleUtil;
 import com.liferay.gradle.plugins.workspace.internal.util.StringUtil;
 import com.liferay.gradle.util.Validator;
 import com.liferay.release.util.ResourceUtil;
 
 import java.io.File;
-
-import java.nio.file.Path;
 
 import java.time.temporal.ChronoUnit;
 
@@ -29,24 +26,10 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-
-import org.gradle.api.Action;
 import org.gradle.api.GradleException;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
-import org.gradle.api.Task;
 import org.gradle.api.logging.Logger;
-import org.gradle.api.tasks.TaskContainer;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
 
 /**
  * @author Drew Brokke
@@ -65,8 +48,6 @@ public class LiferayWorkspaceNodePlugin implements Plugin<Project> {
 		GradleUtil.applyPlugin(project, NodePlugin.class);
 
 		_configureLTS(project);
-
-		_configureTasksPackageRunTest(project);
 	}
 
 	private LiferayWorkspaceNodePlugin() {
@@ -129,29 +110,6 @@ public class LiferayWorkspaceNodePlugin implements Plugin<Project> {
 		);
 	}
 
-	private void _configureTasksPackageRunTest(Project project) {
-		TaskContainer taskContainer = project.getTasks();
-
-		taskContainer.withType(
-			PackageRunTestTask.class,
-			new Action<PackageRunTestTask>() {
-
-				@Override
-				public void execute(PackageRunTestTask packageRunTestTask) {
-					packageRunTestTask.doLast(
-						new Action<Task>() {
-
-							@Override
-							public void execute(Task task) {
-								_writeCITestResults(task.getProject());
-							}
-
-						});
-				}
-
-			});
-	}
-
 	private String _getLts(Project project) {
 		return GradleUtil.getProperty(
 			project, NODE_LTS_PROPERTY_NAME, (String)null);
@@ -200,135 +158,6 @@ public class LiferayWorkspaceNodePlugin implements Plugin<Project> {
 
 		return nodeInfoOptional;
 	}
-
-	private String _getPackageName(Project project) {
-		File projectDir = project.getProjectDir();
-
-		return _getRelativePath(_getRepositoryDir(project), projectDir);
-	}
-
-	private String _getRelativePath(File dir, File file) {
-		Path dirPath = dir.toPath();
-
-		String relativePath = String.valueOf(dirPath.relativize(file.toPath()));
-
-		return relativePath.replace(File.separatorChar, '/');
-	}
-
-	private File _getRepositoryDir(Project project) {
-		File dir = project.getProjectDir();
-
-		while (dir != null) {
-			File gitFile = new File(dir, ".git");
-
-			if (gitFile.exists()) {
-				return dir;
-			}
-
-			dir = dir.getParentFile();
-		}
-
-		return project.getRootDir();
-	}
-
-	private String _getTestClassName(Project project, String testSuiteName) {
-		File testFile = new File(project.getProjectDir(), testSuiteName);
-
-		return _getRelativePath(_getRepositoryDir(project), testFile);
-	}
-
-	private boolean _isTestFileName(String testSuiteName) {
-		if ((testSuiteName == null) || !testSuiteName.contains(".")) {
-			return false;
-		}
-
-		if (testSuiteName.contains("/") || testSuiteName.endsWith(".js") ||
-			testSuiteName.endsWith(".jsx") || testSuiteName.endsWith(".ts") ||
-			testSuiteName.endsWith(".tsx")) {
-
-			return true;
-		}
-
-		return false;
-	}
-
-	private void _writeCITestResults(Project project) {
-		File file = new File(project.getProjectDir(), _TEST_RESULTS_FILE_NAME);
-
-		if (!file.exists()) {
-			return;
-		}
-
-		try {
-			DocumentBuilderFactory documentBuilderFactory =
-				DocumentBuilderFactory.newInstance();
-
-			DocumentBuilder documentBuilder =
-				documentBuilderFactory.newDocumentBuilder();
-
-			Document document = documentBuilder.parse(file);
-
-			NodeList testSuiteNodeList = document.getElementsByTagName(
-				"testsuite");
-
-			boolean modified = false;
-
-			for (int i = 0; i < testSuiteNodeList.getLength(); i++) {
-				Element testSuiteElement = (Element)testSuiteNodeList.item(i);
-
-				String testSuiteName = testSuiteElement.getAttribute("name");
-
-				if (!_isTestFileName(testSuiteName)) {
-					continue;
-				}
-
-				testSuiteElement.setAttribute(
-					"package", _getPackageName(project));
-
-				String testClassName = _getTestClassName(
-					project, testSuiteName);
-
-				NodeList testCaseNodeList =
-					testSuiteElement.getElementsByTagName("testcase");
-
-				for (int j = 0; j < testCaseNodeList.getLength(); j++) {
-					Element testCaseElement = (Element)testCaseNodeList.item(j);
-
-					testCaseElement.setAttribute("classname", testClassName);
-
-					modified = true;
-				}
-			}
-
-			if (!modified) {
-				return;
-			}
-
-			TransformerFactory transformerFactory =
-				TransformerFactory.newInstance();
-
-			Transformer transformer = transformerFactory.newTransformer();
-
-			transformer.transform(
-				new DOMSource(document),
-				new StreamResult(
-					new File(
-						project.getProjectDir(), _CI_TEST_RESULTS_FILE_NAME)));
-		}
-		catch (Exception exception) {
-			Logger logger = project.getLogger();
-
-			if (logger.isWarnEnabled()) {
-				logger.warn("Unable to write CI test results", exception);
-			}
-		}
-	}
-
-	private static final String _CI_TEST_RESULTS_FILE_NAME =
-		"CI_TEST-frontend-js.xml";
-
-	private static final String _TEST_RESULTS_FILE_NAME =
-		"TEST-frontend-js.xml";
 
 	private final NodeInfos _nodeInfos;
 
