50fc3630b83b04dd6b071146c2345d94f5c4530e
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-09-12 14:08:59 -0700

    LRCI-8266 Test the JS unit test class file parser

---
+++ modules/test/jenkins-results-parser/src/test/java/com/liferay/jenkins/results/parser/test/clazz/file/JSUnitTestClassFileTest.java
@@ -0,0 +1,255 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser.test.clazz.file;
+
+import com.liferay.jenkins.results.parser.RandomTestUtil;
+import com.liferay.jenkins.results.parser.ReflectionTestUtil;
+
+import java.io.File;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author Michael Hashimoto
+ */
+public class JSUnitTestClassFileTest
+	extends com.liferay.jenkins.results.parser.Test {
+
+	@Test
+	public void testGetFullName() throws Exception {
+		TestClassFileMethod testClassFileMethod = _getTestClassFileMethod(
+			_getTestClassFile(
+				"describe('a', () => {describe('b', () => {" +
+					"it('c > d', () => {});});});"));
+
+		testEquals("a > b > c > d", testClassFileMethod.getFullName());
+		testEquals("a b c > d", testClassFileMethod.getFullName(" "));
+	}
+
+	@Test
+	public void testGetRelativePath() throws Exception {
+		TestClassFile testClassFile = _getTestClassFile("it('a', () => {});");
+
+		File file = testClassFile.getFile();
+
+		testEquals(
+			file.getName(),
+			testClassFile.getRelativePath(file.getParentFile()));
+	}
+
+	@Test
+	public void testGetTestClassFile() throws Exception {
+		TestClassFile testClassFile = _getTestClassFile("it('a', () => {});");
+
+		TestClassFileMethod testClassFileMethod = _getTestClassFileMethod(
+			testClassFile);
+
+		testSame(testClassFile, testClassFileMethod.getTestClassFile());
+	}
+
+	@Test
+	public void testGetTestMethods() throws Exception {
+		_testGetTestMethods("", new String[0]);
+		_testGetTestMethods(
+			"const a = /alert\\((.*?)\\)/;const b = /'|\"/g;it('c', () => {});",
+			"c");
+		_testGetTestMethods(
+			"describe('a', () => {describe('b', () => {it('c', () => {});});" +
+				"});",
+			"a > b > c");
+		_testGetTestMethods(
+			"describe('a', () => {it('b', () => {});it('c', () => {});});",
+			"a > b", "a > c");
+		_testGetTestMethods(
+			"describe('a', ({b}) => {it('c', () => {});});", "a > c");
+		_testGetTestMethods(
+			"describe.each([['a'], ['b']])('%s', (c) => {" +
+				"it(`${c} d`, () => {});});",
+			"a > a d", "b > b d");
+		_testGetTestMethods("it('a ' + 'b' + \n'c', () => {});", "a bc");
+		_testGetTestMethods(
+			"it('a', () => {render(<b c={{d: 1}} />);render(<e>f</e>);});",
+			"a");
+		_testGetTestMethods("it(`a ${b} c`, () => {});", "a ${b} c");
+		_testGetTestMethods("it(`a`, () => {});", "a");
+		_testGetTestMethods("it(a, () => {});", "${a}");
+		_testGetTestMethods(
+			"it.each(['a', 'b'])('c %s', () => {});", "c a", "c b");
+		_testGetTestMethods("it.each(['a'])('b %p', () => {});", "b %p");
+		_testGetTestMethods("it.each([a.b])('c %s', () => {});", "c %s");
+		_testGetTestMethods("it.each(a)('b %s', () => {});", "b %s");
+		_testGetTestMethods(
+			"test.each(['a'])('%# %% b %s', () => {});", "0 % b a");
+		_testGetTestMethods(
+			"test.each([['a', 'b'], ['c', 'd']])('e %s %s', () => {});",
+			"e a b", "e c d");
+	}
+
+	@Test
+	public void testGetTestMethodsComments() throws Exception {
+		_testGetTestMethods(
+			"/* describe('a', () => {}); */ it('b', () => {});", "b");
+		_testGetTestMethods("// it('a', () => {});\nit('b', () => {});", "b");
+		_testGetTestMethods(
+			"describe('a', () => {// comment\nit('b', () => {});});", "a > b");
+	}
+
+	@Test
+	public void testGetTestMethodsDeclarationWords() throws Exception {
+		String[] describeWords = ReflectionTestUtil.getFieldValue(
+			JSUnitTestClassFile.class, "_DESCRIBE_WORDS");
+
+		for (String describeWord : describeWords) {
+			_testGetTestMethods(
+				describeWord + "('a', () => {it('b', () => {});});", "a > b");
+		}
+
+		String[] testWords = ReflectionTestUtil.getFieldValue(
+			JSUnitTestClassFile.class, "_TEST_WORDS");
+
+		for (String testWord : testWords) {
+			_testGetTestMethods(testWord + "('a', () => {});", "a");
+		}
+	}
+
+	@Test
+	public void testGetTestMethodsEscapes() throws Exception {
+		_testGetTestMethods("it('a\\'b', () => {});", "a'b");
+		_testGetTestMethods("it('a\\nb', () => {});", "a\nb");
+		_testGetTestMethods("it(\"a\\\"b\", () => {});", "a\"b");
+	}
+
+	@Test
+	public void testGetTestMethodsExpressionTitles() throws Exception {
+		_testGetTestMethods("it(() => {}, () => {});", new String[0]);
+		_testGetTestMethods("it(a('b, c'), () => {});", "${a('b, c')}");
+		_testGetTestMethods("it(a.b, () => {});", "${a.b}");
+		_testGetTestMethods("it(a[0], () => {});", "${a[0]}");
+	}
+
+	@Test
+	public void testIsDynamic() throws Exception {
+		_testIsDynamic("it('a', () => {});", false);
+		_testIsDynamic("it(`a ${b}`, () => {});", true);
+		_testIsDynamic("it(`a`, () => {});", false);
+		_testIsDynamic("it(a, () => {});", true);
+		_testIsDynamic("it.each(['a'])('b %p', () => {});", true);
+		_testIsDynamic("it.each(['a'])('b %s', () => {});", false);
+		_testIsDynamic("it.each([a.b])('c %s', () => {});", true);
+	}
+
+	@Test
+	public void testMatches() throws Exception {
+		TestClassFileMethod testClassFileMethod = _getTestClassFileMethod(
+			_getTestClassFile("describe('a', () => {it('b', () => {});});"));
+
+		Assert.assertFalse(testClassFileMethod.matches("a-b"));
+		Assert.assertFalse(
+			testClassFileMethod.matches(RandomTestUtil.randomString()));
+		Assert.assertFalse(testClassFileMethod.matches(null));
+		Assert.assertTrue(testClassFileMethod.matches("a > b"));
+		Assert.assertTrue(testClassFileMethod.matches("a b"));
+	}
+
+	@Test
+	public void testMatchesPattern() throws Exception {
+		TestClassFile testClassFile = _getTestClassFile(
+			"describe('a', () => {it(`b ${c} d`, () => {});" +
+				"it('e', () => {});});");
+
+		List<TestClassFileMethod> testClassFileMethods =
+			testClassFile.getTestMethods();
+
+		TestClassFileMethod dynamicTestClassFileMethod =
+			testClassFileMethods.get(0);
+
+		Assert.assertFalse(dynamicTestClassFileMethod.matchesPattern("a > b"));
+		Assert.assertFalse(dynamicTestClassFileMethod.matchesPattern(null));
+		Assert.assertTrue(
+			dynamicTestClassFileMethod.matchesPattern("a > b anything d"));
+		Assert.assertTrue(
+			dynamicTestClassFileMethod.matchesPattern("a b anything d"));
+
+		TestClassFileMethod testClassFileMethod = testClassFileMethods.get(1);
+
+		Assert.assertFalse(
+			testClassFileMethod.matchesPattern(RandomTestUtil.randomString()));
+		Assert.assertTrue(testClassFileMethod.matchesPattern("a > e"));
+	}
+
+	@Test
+	public void testNewTestClassFile() throws Exception {
+		File file = File.createTempFile(RandomTestUtil.randomString(), ".txt");
+
+		try {
+			Assert.assertNull(TestClassFileFactory.newTestClassFile(file));
+			Assert.assertNull(
+				TestClassFileFactory.newTestClassFile(file.getParentFile()));
+		}
+		finally {
+			file.delete();
+		}
+	}
+
+	private TestClassFile _getTestClassFile(String content) throws Exception {
+		File file = File.createTempFile(
+			RandomTestUtil.randomString(), ".spec.tsx");
+
+		try {
+			Files.write(
+				file.toPath(), content.getBytes(StandardCharsets.UTF_8));
+
+			return TestClassFileFactory.newTestClassFile(file);
+		}
+		finally {
+			file.delete();
+		}
+	}
+
+	private TestClassFileMethod _getTestClassFileMethod(
+		TestClassFile testClassFile) {
+
+		List<TestClassFileMethod> testClassFileMethods =
+			testClassFile.getTestMethods();
+
+		return testClassFileMethods.get(0);
+	}
+
+	private void _testGetTestMethods(
+			String content, String... expectedFullNames)
+		throws Exception {
+
+		List<String> fullNames = new ArrayList<>();
+
+		TestClassFile testClassFile = _getTestClassFile(content);
+
+		for (TestClassFileMethod testClassFileMethod :
+				testClassFile.getTestMethods()) {
+
+			fullNames.add(testClassFileMethod.getFullName());
+		}
+
+		testEquals(Arrays.asList(expectedFullNames), fullNames);
+	}
+
+	private void _testIsDynamic(String content, boolean expectedDynamic)
+		throws Exception {
+
+		TestClassFileMethod testClassFileMethod = _getTestClassFileMethod(
+			_getTestClassFile(content));
+
+		testEquals(expectedDynamic, testClassFileMethod.isDynamic());
+	}
+
+}
