fe1b8044eb130d4d0a716cacb5600012425c1015
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-09-18 18:39:44 -0700

    LRCI-8297 Add a jest junit reporter to use within CI

--- modules/frontend-sdk/node-scripts/util/jest/getJestConfig.js
+++ modules/frontend-sdk/node-scripts/util/jest/getJestConfig.js
@@ -51,7 +51,10 @@ function getJestConfig({rootDir = '<rootDir>'}) {
 			`${rootDir}/test/stories/`,
 			'/test/__lib__/',
 		],
-		testResultsProcessor: '@liferay/jest-junit-reporter',
+		testResultsProcessor:
+			process.env.JEST_JUNIT_REPORTER === 'ci'
+				? path.join(__dirname, 'jestJUnitReporter.js')
+				: '@liferay/jest-junit-reporter',
 		transform: {
 
 			/* eslint-disable sort-keys */
