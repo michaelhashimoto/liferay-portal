cf41c339708631d27ccb3de0e1b4f51bab3d6ebc
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-09-18 18:38:42 -0700

    LRCI-8297 Add a jest junit reporter to use within CI

--- modules/frontend-sdk/node-scripts/util/jest/getJestConfig.js
+++ modules/frontend-sdk/node-scripts/util/jest/getJestConfig.js
@@ -1,5 +1,5 @@
 /**
- * SPDX-FileCopyrightText: (c) 2000 Liferay, Inc. https://liferay.com
+ * SPDX-FileCopyrightText: (c) 2026 Liferay, Inc. https://liferay.com
  * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
  */
 
@@ -51,7 +51,10 @@ function getJestConfig({rootDir = '<rootDir>'}) {
 			`${rootDir}/test/stories/`,
 			'/test/__lib__/',
 		],
-		testResultsProcessor: '@liferay/jest-junit-reporter',
+		testResultsProcessor:
+			process.env.JEST_JUNIT_REPORTER === 'ci'
+				? path.join(__dirname, 'jestJUnitReporter.js')
+				: '@liferay/jest-junit-reporter',
 		transform: {
 
 			/* eslint-disable sort-keys */
