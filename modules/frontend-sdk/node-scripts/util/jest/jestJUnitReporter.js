e567024df6f8804bce6d82405e41000a6f698a9c
Michael Hashimoto <michael.hashimoto@liferay.com>
2026-09-21 10:16:04 -0700

    LRCI-8297 Generate the jest JUnit report without strip-ansi and xml
    The reporter only needs to strip ANSI escapes and emit a flat testsuite document, both of which Node covers on its own. Dropping the two dependencies removes a pair of packages from the workspace install for no loss in output.

--- modules/frontend-sdk/node-scripts/util/jest/jestJUnitReporter.js
+++ modules/frontend-sdk/node-scripts/util/jest/jestJUnitReporter.js
@@ -5,12 +5,19 @@
 
 const fs = require('fs');
 const path = require('path');
-const stripAnsi = require('strip-ansi');
-const xml = require('xml');
+const {stripVTControlCharacters} = require('util');
 
 const PROJECT_DIR = process.cwd();
 
 const SKIPPED_STATUSES = ['pending', 'todo'];
+
+const XML_CHARACTER_MAP = {
+	'"': '&quot;',
+	'&': '&amp;',
+	"'": '&apos;',
+	'<': '&lt;',
+	'>': '&gt;',
+};
 
 function countFailures(testCases) {
 	const failedTestCases = testCases.filter(
@@ -27,12 +34,67 @@ function countSkipped(testCases) {
 	return skippedTestCases.length;
 }
 
+function escapeXML(value) {
+	return String(value).replace(
+		/["&'<>]/g,
+		(character) => XML_CHARACTER_MAP[character]
+	);
+}
+
+function formatAttributes(attributes) {
+	return Object.entries(attributes)
+		.map(([name, value]) => ` ${name}="${escapeXML(value)}"`)
+		.join('');
+}
+
 function formatDuration(startTime) {
 	if (!startTime) {
 		return 0;
 	}
 
 	return (Date.now() - startTime) / 1000;
+}
+
+function formatFailureMessages(testCase) {
+	return testCase.failureMessages.map((failureMessage) =>
+		stripVTControlCharacters(failureMessage)
+			.split(PROJECT_DIR + path.sep)
+			.join('')
+	);
+}
+
+function formatTestCase(testCase) {
+	const attributes = formatAttributes({
+		classname: formatTestFilePath(testCase.testFilePath),
+		name: formatTestName(testCase),
+		time: testCase.duration / 1000,
+	});
+
+	const children = [];
+
+	if (isSkipped(testCase)) {
+		children.push('\t\t<skipped/>');
+	}
+
+	if (testCase.failureMessages && testCase.failureMessages.length) {
+		const failureMessages = formatFailureMessages(testCase);
+
+		const failureAttributes = formatAttributes({
+			message: failureMessages[0].split('\n')[0],
+		});
+
+		children.push(
+			`\t\t<failure${failureAttributes}>${escapeXML(
+				failureMessages.join('\n')
+			)}</failure>`
+		);
+	}
+
+	if (!children.length) {
+		return [`\t<testcase${attributes}/>`];
+	}
+
+	return [`\t<testcase${attributes}>`, ...children, '\t</testcase>'];
 }
 
 function formatTestFilePath(testFilePath) {
@@ -81,72 +143,27 @@ function isSkipped(testCase) {
 module.exports = (testReport) => {
 	const testCases = getTestCases(testReport);
 
-	const testResults = testCases.map((testCase) => {
-		const testResults = [
-			{
-				_attr: {
-					classname: formatTestFilePath(testCase.testFilePath),
-					name: formatTestName(testCase),
-					time: testCase.duration / 1000,
-				},
-			},
-		];
-
-		if (isSkipped(testCase)) {
-			testResults.push({skipped: null});
-		}
-
-		if (testCase.failureMessages && testCase.failureMessages.length) {
-			const failureMessageArray = testCase.failureMessages.map(
-				(failureMessage) =>
-					failureMessage.split('\n').map((message) =>
-						stripAnsi(message)
-							.split(PROJECT_DIR + path.sep)
-							.join('')
-					)
-			);
-
-			testResults.push({
-				failure: [
-					{
-						_attr: {
-							message: failureMessageArray[0][0],
-						},
-					},
-					failureMessageArray
-						.map((failureMessage) => failureMessage.join('\n'))
-						.join('\n'),
-				],
-			});
-		}
-
-		return {
-			testcase: testResults,
-		};
+	const testSuiteAttributes = formatAttributes({
+		errors: 0,
+		failures: countFailures(testCases),
+		hostname: '',
+		id: 0,
+		name: 'Jest',
+		package: PROJECT_DIR,
+		skipped: countSkipped(testCases),
+		tests: testCases.length,
+		time: formatDuration(testReport.startTime),
+		timestamp: testReport.startTime,
 	});
 
-	const testSuiteAttributes = {
-		_attr: {
-			errors: 0,
-			failures: countFailures(testCases),
-			hostname: '',
-			id: 0,
-			name: 'Jest',
-			package: PROJECT_DIR,
-			skipped: countSkipped(testCases),
-			tests: testCases.length,
-			time: formatDuration(testReport.startTime),
-			timestamp: testReport.startTime,
-		},
-	};
+	const lines = [
+		'<?xml version="1.0" encoding="UTF-8"?>',
+		`<testsuite${testSuiteAttributes}>`,
+		...testCases.flatMap(formatTestCase),
+		'</testsuite>',
+	];
 
-	fs.writeFileSync(
-		'TEST-frontend-js.xml',
-		xml(
-			{testsuite: [testSuiteAttributes, ...testResults]},
-			{declaration: true, indent: '  '}
-		)
-	);
+	fs.writeFileSync('TEST-frontend-js.xml', lines.join('\n'));
 
 	return testReport;
 };
